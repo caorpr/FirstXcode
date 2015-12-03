a42ace5708506b4fec2970ee890a5e364a6ac35b
caorpr <caorpr@gmail.com>
2015-12-03 13:57:23 -0700

    fdsafdasfds

---
+++ Sample.swift
@@ -0,0 +1,9 @@
+//
+//  Sample.swift
+//  FirstXcode
+//
+//  Created by Anderson, Clayton on 12/1/15.
+//
+//
+
+import Foundation
---
+++ Stffds.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+var someVariable :String
+someVariable = 71823
+someVariable = "junk"
+
+print(str)
+var
+
